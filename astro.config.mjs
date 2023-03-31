6cbd6d458466ece0d5d479e04f04dda5077bc399
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-03-31 22:03:36 +0800

    feat: add markdoc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import deno from "@astrojs/deno";
-
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import prefetch from "@astrojs/prefetch";
 import image from "@astrojs/image";
 
+import markdoc from "@astrojs/markdoc";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: deno(),
-  integrations: [tailwind(), react(), prefetch(), image()]
+  integrations: [tailwind(), react(), prefetch(), image(), markdoc()]
 });
