850b67fe6953863d2a03d482806735f8af1af51c
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-04-02 23:33:54 +0800

    feat: add Card

--- src/components/CardList/Card.tsx
+++ src/components/CardList/Card.tsx
@@ -10,12 +10,6 @@ export const Card = ({ title, description, type, url, tags }: cardProps) => {
   return (
     <div className="mb-6 md:mb-0 text-left relative card hover:shadow-lg transition-shadow tbc">
       <div className="relative z-40 pointer-events-none p-6 pb-0 md:pb-0">
-        <div className="flex items-center justify-between mb-3">
-          <div></div>
-          <div className="text-xs capitalize text-zinc-50 px-3 py-1 rounded-full bg-primary">
-            {type}
-          </div>
-        </div>
         <h3 className="mt-0 text-2xl mb-1">
           <a
             className="font-bold text-zinc-900 pointer-events-auto dark:text-white"
@@ -30,11 +24,14 @@ export const Card = ({ title, description, type, url, tags }: cardProps) => {
           </p>
         </div>
         <div className="w-full h-auto flex flex-wrap mb-6 overflow-hidden gap-2">
+          <div className="text-xs capitalize text-zinc-50 px-3 py-1 rounded-full bg-primary">
+            {type}
+          </div>
           {Array.isArray(tags) &&
             tags?.length > 0 &&
             tags.map((item) => {
               return (
-                <div className="text-xs capitalize text-zinc-50 px-3 py-1 rounded-full bg-primary">
+                <div key={item} className="text-xs capitalize text-zinc-50 px-3 py-1 rounded-full bg-primary">
                   {item}
                 </div>
               );
