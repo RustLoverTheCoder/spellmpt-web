2cf35460075888712f301a12c1b4672ad6a9d86b
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-04-05 02:19:38 +0800

    feat: add static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import deno from "@astrojs/deno";
+// import deno from "@astrojs/deno";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import prefetch from "@astrojs/prefetch";
@@ -8,7 +8,7 @@ import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: deno(),
+  output: "static",
+  // adapter: deno(),
   integrations: [tailwind(), react(), prefetch(), markdoc()]
 });
