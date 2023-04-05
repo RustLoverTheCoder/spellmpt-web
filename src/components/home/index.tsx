d7e49ff7b44879fbc2610a80b819c6a40a0cd381
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-04-05 20:29:21 +0800

    feat: add Prompt index

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -4,10 +4,10 @@ const page = () => {
       <section className="px-4 md:px-0 py-12 md:pt-36 mt:pb-10">
         <div className="md:max-w-4xl lg:max-w-7xl text-center mx-auto w-11/12 mb-10">
           <h1 className="text-4xl md:text-5xl lg:text-6xl mb-6 max-w-4xl mx-auto font-bold">
-            Universal Micro Services
+            Free Prompt Library
           </h1>
           <h2 className="text-md md:text-lg max-w-2xl mx-auto">
-            Explore the cloud as serverless building blocks
+            告别无效提示，释放 AI 的全部潜力
           </h2>
           <div className="mt-10 md:flex items-center max-w-lg mx-auto">
             <a
