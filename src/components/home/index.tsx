92995917d00f4c4916b45f53332b1a0ee7510deb
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-04-05 22:19:44 +0800

    feat: remove free button

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -4,19 +4,12 @@ const page = () => {
       <section className="px-4 md:px-0 py-12 md:pt-36 mt:pb-10">
         <div className="md:max-w-4xl lg:max-w-7xl text-center mx-auto w-11/12 mb-10">
           <h1 className="text-4xl md:text-5xl lg:text-6xl mb-6 max-w-4xl mx-auto font-bold">
-            Free Prompt Library
+            Best Prompt Library
           </h1>
           <h2 className="text-md md:text-lg max-w-2xl mx-auto">
             告别无效提示，释放 AI 的全部潜力
           </h2>
-          <div className="mt-10 md:flex items-center max-w-lg mx-auto">
-            <a
-              href="/explore"
-              className="inline-flex items-center justify-center btn w-full text-center mb-4  md:w-auto md:mx-auto btn-primary"
-            >
-              Start for Free
-            </a>
-          </div>
+          <div className="mt-10 md:flex items-center max-w-lg mx-auto"></div>
         </div>
       </section>
     </main>
